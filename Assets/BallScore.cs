65feffe69eb66db56c5ebbac144fe24c3b2f600b
jumpeioki <joki@sun-denshi.co.jp>
2018-06-16 16:07:08 +0900

    Lesson５の課題です。

---
+++ Assets/BallScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallScore : MonoBehaviour {
+
+	private int score = 0;
+	private GameObject pointText;
+
+	void Start () {
+		this.pointText = GameObject.Find("PointText");
+	}
+
+	void Update () {
+		this.pointText.GetComponent<Text>().text = score.ToString();
+
+	}
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "SmallStarTag") {
+			score += 10;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			score += 30;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			score += 20;
+		} else if (other.gameObject.tag == "LargeCloudTag"){
+			score += 40;
+		}
+
+	}
+}
---
+++ Assets/CloudController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour {
+
+	private float minimum = 1.0f;
+	private float magspeed = 10.0f;
+	private float magnification = 0.07f;
+
+	void Start (){
+	}
+
+	void Update(){
+		this.transform.localScale = new Vector3(this.minimum + Mathf.Sin(Time.time * this.magspeed) * this.magnification, this.transform.localScale.y, this.minimum +　Mathf.Sin(Time.time * this.magspeed) * this.magnification);
+	}
+
+}
